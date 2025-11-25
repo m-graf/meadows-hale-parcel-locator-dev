6096d206dcabf723cc548e7f9b1d61360c842391
m-graf <michael@gr.af>
2025-11-25 10:11:25 -0600

    Initial Commit

---
+++ PrincipalAcres/PrincipalAcresApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrincipalAcresApp.swift
+//  PrincipalAcres
+//
+//  Created by mgraf on 11/25/25.
+//
+
+import SwiftUI
+
+@main
+struct PrincipalAcresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
